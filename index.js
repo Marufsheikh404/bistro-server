3b872b252bc09bc1ce12b4eb46f61889775d8ae0
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-10-14 22:43:20 +0600

    jwt is done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,20 +33,68 @@ async function run() {
         const cardCollection = client.db('BistroDB').collection('carts');
         const userCollection = client.db('BistroDB').collection('users');
 
-        // user related apis
-        app.get('/users',async(req,res)=>{
-            const result = await userCollection.find().toArray();
-            res.send(result);
+
+        // middleWire
+        const verifyToken = (req,res, next)=>{
+            console.log("verifyToken", req.headers);
+            if(!req.headers.authorization){
+                return res.status(401).send({message:"forbidden access"});
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN,(err,decoded)=>{
+                if(err){
+                  return res.status(401).send({message:'forbidden access'})
+                }
+                req.decoded = decoded;
+            })
+            next();
+        }
+
+
+        // jwt related apis
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+            res.send({ token })
         })
 
 
-        app.post('/users',async(req,res)=>{
+        // admin related apis
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result)
+        });
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        });
+
+
+
+        // user related apis
+        app.get('/users',verifyToken, async (req, res) => {
+            console.log('Authorization token:', req.headers.authorization);
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post('/users', async (req, res) => {
             const data = req.body;
             // we check google user is already in database nki
-            const query ={email: data.email} 
+            const query = { email: data.email }
             const exisitingUser = await userCollection.findOne(query);
-            if(exisitingUser){
-                return res.send({message:'user already exists', insertedId:null})
+            if (exisitingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
             }
             const result = await userCollection.insertOne(data);
             res.send(result);
@@ -53,33 +102,33 @@ async function run() {
 
 
 
-        app.get('/Menu',async(req,res)=>{
+        app.get('/Menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result)
         });
 
-        app.get('/Reviews',async(req,res)=>{
+        app.get('/Reviews', async (req, res) => {
             const results = await reviweCollection.find().toArray();
             res.send(results);
         });
 
         // card related apis
-        app.post('/carts',async(req,res)=>{
+        app.post('/carts', async (req, res) => {
             const cardItem = req.body;
             const result = await cardCollection.insertOne(cardItem);
             res.send(result)
         });
 
-        app.get('/carts',async(req,res)=>{
+        app.get('/carts', async (req, res) => {
             const email = req.query.email;
-            const query = {email : email}
+            const query = { email: email }
             const result = await cardCollection.find(query).toArray();
             res.send(result);
         });
 
-        app.delete('/carts/:id', async(req,res)=>{
-            const id= req.params.id;
-            const query = {_id: new ObjectId(id)};
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
             const result = await cardCollection.deleteOne(query);
             res.send(result)
         });
